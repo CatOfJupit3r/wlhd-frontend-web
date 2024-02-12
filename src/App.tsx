82b2823573e6b301e441257718b0686b25c6ef8b
catjofupit3r <catofjupit3r@gmail.com>
2024-02-12 10:47:01 +0200

    outlined basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
