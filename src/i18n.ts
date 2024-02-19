c8bfb382c8acebad554deab66afa6e84a87214e7
CatOfJupit3r <catofjupit3r@gmail.com>
2024-02-19 23:09:31 +0200

    stuff

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 
-// const resources: object = getLanguageFiles()
+console.log(getLanguageFiles())
 
 
 i18next
--- src/utils/languageLoader.ts
+++ src/utils/languageLoader.ts
@@ -1,5 +1,43 @@
+import fs from 'fs'
+
+
 function getLanguageFiles() {
-    return null
+    /* we open folder translations and go through each folder.
+    In each folder, there are subfolder representing languages.
+    In those subfolders, there are json files representing the translations.
+
+    We get jsons, add them to dlc category (which is located in manifest.json) and dlc category to the language
+    * */
+    console.log("111")
+    const languages = {}
+    const dlc = {}
+    const translations = {}
+    const translationPath = '../translations/'
+    const folders = fs.readdirSync(translationPath)
+    console.log(folders)
+    for (const folder of folders) {
+        const languagePath = translationPath + folder
+        const languageFolders = fs.readdirSync(languagePath)
+        console.log(folder)
+        // for (const languageFolder of languageFolders) {
+        //     const languageFiles = fs.readdirSync(languagePath + '/' + languageFolder)
+        //     for (const languageFile of languageFiles) {
+        //         const language = languageFile.split('.')[0]
+        //         const languageJson = JSON.parse(fs.readFileSync(languagePath + '/' + languageFolder + '/' + languageFile, 'utf8'))
+        //         if (!languages[language]) {
+        //             languages[language] = {}
+        //         }
+        //         languages[language][folder] = languageJson
+        //         if (!dlc[folder]) {
+        //             dlc[folder] = []
+        //         }
+        //         dlc[folder].push(language)
+        //         translations[folder] = languages
+        //     }
+        // }
+    }
+    return 1
 }
 
-export default getLanguageFiles()
+
+export default getLanguageFiles
