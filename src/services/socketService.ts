1211471b3d1f01d76e7e4f16e06ec4b0e19b55ea
CatOfJupit3r <catofjupit3r@gmail.com>
2024-03-02 20:51:33 +0200

    chores: removed unnecessary stuff, that moved to backend

--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -14,40 +14,18 @@ In plans:
 - Handle errors.
  */
 
-
-const VERIFICATION = (game_id: string) => {
-    return {
-        "command": "add_socket",
-        "payload": {
-            "species": "web",
-            game_id,
-            "players": ["100001"]
+const connectToSocket = (gameId: string, userToken: string): Socket => {
+    const socket = io('http://localhost:3001', {
+        query: {
+            game_id: gameId,
+            user_token: userToken
         }
-    }
-}
-
-
-export const connectToServer = () => {
-    const socket = io();
-
-    // Connect to the server
+    });
     socket.on('connect', () => {
-        console.log('Connected to server');
-        socket.emit("json_data", VERIFICATION("5000"));
+        console.log('Connected to game server');
     });
-
+    socket.on('error', (data: any) => {
+        console.error('error', data);
+    });
     return socket;
-};
-
-export const handleIncomingData = (socket: Socket, callback: (data: any) => void) => {
-    socket.on('json_data', (data: any) => {
-        console.log('Received JSON data:', data);
-        callback(data);
-    });
-};
-
-export const extractKeysFromJSON = (data: any): { command: string, keys: string[] } => {
-    const command = data.command;
-    const keys = Object.keys(data).filter(key => key !== 'command');
-    return { command, keys };
-};
+}
