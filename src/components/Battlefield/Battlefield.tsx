6c3b2f46f6df7dd3ae66894ffad7e152a70445ea
CatOfJupit3r <catofjupit3r@gmail.com>
2024-03-06 20:35:33 +0200

    feature: added blurhash in case battlefield is undefined

--- src/components/Battlefield/Battlefield.tsx
+++ src/components/Battlefield/Battlefield.tsx
@@ -3,6 +3,8 @@ import {parseBattlefield, parsedToJSX} from "./utils";
 import styles from "./Battlefield.module.css"
 import {useSelector} from "react-redux";
 import {selectCurrentBattlefield} from "../../redux/slices/infoSlice";
+import {Blurhash} from "react-blurhash";
+import {BATTLEFIELD_BLUR_HASH} from "../../config/configs";
 
 const Battlefield = () => {
 
@@ -10,7 +12,11 @@ const Battlefield = () => {
 
     return (
         <div className={styles.battlefield} id={"battlefield-div"}>
-            {parsedToJSX(parseBattlefield(battlefield))}
+            {!battlefield ?
+                <Blurhash hash={BATTLEFIELD_BLUR_HASH} width={64 * 8} height={64 * 9} />
+                :
+                parsedToJSX(parseBattlefield(battlefield))
+            }
         </div>
     );
 };
