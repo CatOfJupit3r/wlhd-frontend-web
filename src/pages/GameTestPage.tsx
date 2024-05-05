aa3f4013116aad5f531d8de03bb7ae9a8d6f8318
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-05 23:24:48 +0300

    feat: merge example jsons into example_gamestate.json.

--- src/pages/GameTestPage.tsx
+++ src/pages/GameTestPage.tsx
@@ -1,14 +1,28 @@
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
-import { resetTurnSlice } from '../redux/slices/turnSlice'
+import { resetTurnSlice, setEntityActions, setPlayersTurn } from '../redux/slices/turnSlice'
 import GameScreen from '../components/GameScreen/GameScreen'
-
+import { AppDispatch } from '../redux/store'
+import { ActionInput } from '../models/ActionInput'
+import { setBattlefield } from '../redux/slices/battlefieldSlice'
+import { EntityInfoFull, EntityInfoTurn, GameStateContainer } from '../models/Battlefield'
+import example_gamestate from '../data/example_gamestate.json'
+import { setActiveEntity, setControlledEntities, setMessages } from '../redux/slices/infoSlice'
 
 const GameTestPage = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch<AppDispatch>()
 
     useEffect(() => {
         dispatch(resetTurnSlice()) // to prevent any leftover state from previous games
+    }, [dispatch])
+
+    useEffect(() => {
+        dispatch(setMessages(example_gamestate.messages as GameStateContainer))
+        dispatch(setControlledEntities(example_gamestate.controlledEntities as EntityInfoFull[]))
+        dispatch(setActiveEntity(example_gamestate.activeEntity as EntityInfoTurn))
+        dispatch(setBattlefield(example_gamestate.battlefield))
+        dispatch(setEntityActions(example_gamestate.actions as ActionInput))
+        dispatch(setPlayersTurn(true))
     }, [dispatch])
 
     return (
