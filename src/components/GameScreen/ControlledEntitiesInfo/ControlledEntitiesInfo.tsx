d9268a0b9b868460ab4e436a296a617bd802b40c
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-06 23:47:56 +0300

    feat: add .module.css for <ControlledEntitiesInfo />.

--- src/components/GameScreen/ControlledEntitiesInfo/ControlledEntitiesInfo.tsx
+++ src/components/GameScreen/ControlledEntitiesInfo/ControlledEntitiesInfo.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import { useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { selectControlledEntities } from '../../../redux/slices/infoSlice'
+import EntityDisplay from '../../EntityDisplay/EntityDisplay'
+import styles from './ControlledEntitiesInfo.module.css'
 
 const ControlledEntitiesInfo = () => {
     const controlledEntities = useSelector(selectControlledEntities)
 
-    return (<div>
-            <h1>Controlled Entities Info</h1>
+    const noEntities = useCallback(() => {
+        return <p>You have no entities</p>
+    }, [])
+
+    return (
+        <div className={styles.entitiesContainer}>
+            {controlledEntities
+                ? controlledEntities.length > 0
+                    ? controlledEntities.map((entity, index) => (
+                          <div key={index}>
+                              <EntityDisplay entityInfo={entity} />
+                          </div>
+                      ))
+                    : noEntities()
+                : noEntities()}
         </div>
     )
 }
