46a379471ac93c26bc1b2ef8fca408b496cc1116
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-07 21:38:52 +0300

    feat: add indicator if its players turn.

--- src/components/GameScreen/RoundHeader.tsx
+++ src/components/GameScreen/RoundHeader.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import {useSelector} from "react-redux";
-import {selectRound} from "../../redux/slices/infoSlice";
+import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { selectRound } from '../../redux/slices/infoSlice'
+import { selectPlayersTurn } from '../../redux/slices/turnSlice'
 
 const RoundHeader = () => {
+    const { t } = useTranslation()
+
+    const isPlayerTurn = useSelector(selectPlayersTurn)
     const round = useSelector(selectRound)
 
     return (
         <div>
-            <h1>Round {round}</h1>
+            <h1>
+                {isPlayerTurn
+                    ? `${t('local:game.round_n', { round })}. ${t('local:game.its_your_turn')}`
+                    : `${t('local:game.round_n', { round })}`}
+            </h1>
         </div>
     )
 }
