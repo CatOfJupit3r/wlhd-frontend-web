c3934233d771881f3c08dc28dd1f404b576e527a
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-19 22:03:31 +0300

    chore: update example data

--- src/pages/GameTestPage.tsx
+++ src/pages/GameTestPage.tsx
@@ -1,34 +1,48 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { resetTurnSlice, setEntityActions, setPlayersTurn } from '../redux/slices/turnSlice'
 import GameScreen from '../components/GameScreen/GameScreen'
-import { AppDispatch } from '../redux/store'
-import { ActionInput } from '../models/ActionInput'
-import { setBattlefield } from '../redux/slices/battlefieldSlice'
-import { EntityInfoFull, EntityInfoTurn, GameStateContainer } from '../models/Battlefield'
 import example_gamestate from '../data/example_gamestate.json'
+import { EntityInfoFull } from '../models/Battlefield'
+import { setBattlefield } from '../redux/slices/battlefieldSlice'
 import { setActiveEntity, setControlledEntities, setEntityTooltips, setMessages } from '../redux/slices/infoSlice'
+import { resetTurnSlice, setEntityActions, setPlayersTurn } from '../redux/slices/turnSlice'
+import { AppDispatch } from '../redux/store'
+import APIService from '../services/APIService'
 
 const GameTestPage = () => {
     const dispatch = useDispatch<AppDispatch>()
+    const { i18n } = useTranslation()
+
+    const loadTranslations = useCallback(async () => {
+        const translations = await APIService.getTranslations([i18n.language, 'uk-UA'], ['builtins', 'nyrzamaer'])
+        if (!translations) throw new Error('Failed to load translations. Server response was empty.')
+        console.log('translations', translations)
+        for (const language in translations) {
+            if (translations[language] === null) continue
+            for (const dlc in translations[language]) {
+                if (translations[language][dlc] === null) continue
+                i18n.addResourceBundle(language, dlc, translations[language][dlc], true, true)
+            }
+        }
+    }, [i18n])
 
     useEffect(() => {
         dispatch(resetTurnSlice()) // to prevent any leftover state from previous games
+        loadTranslations().then(() => console.log('translations loaded')).catch((error) => console.log(error))
     }, [dispatch])
 
     useEffect(() => {
-        // dispatch(setEntityTooltips(example_gamestate.tooltips))
-        // dispatch(setMessages(example_gamestate.messages as GameStateContainer))
-        // dispatch(setControlledEntities(example_gamestate.controlledEntities as EntityInfoFull[]))
-        // dispatch(setActiveEntity(example_gamestate.activeEntity as EntityInfoTurn))
-        // dispatch(setBattlefield(example_gamestate.battlefield))
-        // dispatch(setEntityActions(example_gamestate.actions as ActionInput))
-        // dispatch(setPlayersTurn(true))
+        dispatch(setEntityTooltips(example_gamestate.tooltips) as any)
+        dispatch(setMessages(example_gamestate.messages as any))
+        dispatch(setControlledEntities(example_gamestate.controlledEntities as EntityInfoFull[]))
+        dispatch(setActiveEntity(example_gamestate.activeEntity as any))
+        dispatch(setBattlefield(example_gamestate.battlefield as any))
+        dispatch(setEntityActions(example_gamestate.actions as any))
+        dispatch(setPlayersTurn(true))
     }, [dispatch])
 
-    return (
-        <GameScreen />
-    )
+    return <GameScreen />
 }
 
 export default GameTestPage
