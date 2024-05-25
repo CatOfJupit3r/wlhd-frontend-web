c82fcc756ea7a733a22ccf66c3b6e646a5a56715
CatOfJupit3r <catofjupit3r@gmail.com>
2024-05-25 17:24:06 +0300

    chore: optimize `<Header />` for mobile layouts. improve its code readability.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,59 +1,64 @@
-import React from 'react'
+import { useCallback } from 'react'
 import { Link } from 'react-router-dom'
-import styles from './Header.module.css'
-import AuthManager from '../../services/AuthManager'
 import useIsLoggedIn from '../../hooks/useIsLoggedIn'
 import paths from '../../router/paths'
+import AuthManager from '../../services/AuthManager'
+import styles from './Header.module.css'
 
 const Header = () => {
     const { isLoggedIn } = useIsLoggedIn()
 
+    const AuthLinks = useCallback(() => {
+        return (
+            <>
+                <Link to={paths.signIn} relative={'path'}>
+                    Sign In
+                </Link>
+                <Link to={paths.signUp} relative={'path'}>
+                    Sign Up
+                </Link>
+            </>
+        )
+    }, [])
+
+    const LoggedInLinks = useCallback(() => {
+        return (
+            <>
+                <Link to={paths.profile} relative={'path'}>
+                    Profile
+                </Link>
+                <Link
+                    to={'.'}
+                    onClick={(e) => {
+                        e.preventDefault()
+                        AuthManager.logout()
+                    }}
+                >
+                    Logout
+                </Link>
+            </>
+        )
+    }, [])
+
+    const Navigation = useCallback(() => {
+        return <nav id={'header-nav'}>{isLoggedIn ? <LoggedInLinks /> : <AuthLinks />}</nav>
+    }, [isLoggedIn])
+
     return (
         <header className={styles.header}>
-            <p
-                style={{
-                    display: 'flex',
-                }}
+            <Link
+                to="."
+                relative={'route'}
+                id={'header-logo'}
             >
                 Walenholde
-            </p>
+            </Link>
             {/* you served well, o' feline. For this, you will be engraved here until the end of times. */}
             {/*<img src={"assets/local/cat_eat.gif"} alt="cat" style={{*/}
             {/*    height: "400px",*/}
             {/*    width: "100%"*/}
             {/*}}/>*/}
-            <nav>
-                <Link to="." relative={'route'} style={{ marginRight: '10px' }}>
-                    Home
-                </Link>
-                <Link to="about" relative={'path'} style={{ marginRight: '10px' }}>
-                    About
-                </Link>
-                {isLoggedIn ? (
-                    <>
-                        <Link to="profile" relative={'path'} style={{ marginRight: '10px' }}>
-                            Profile
-                        </Link>
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault()
-                                AuthManager.logout()
-                            }}
-                        >
-                            Logout
-                        </button>
-                    </>
-                ) : (
-                    <>
-                        <Link to={paths.signIn} relative={'path'} style={{ marginRight: '10px' }}>
-                            Sign In
-                        </Link>
-                        <Link to={paths.signUp} relative={'path'}>
-                            Sign Up
-                        </Link>
-                    </>
-                )}
-            </nav>
+            <Navigation />
         </header>
     )
 }
