df612b8e7fb1a7f03bbab8e62dae2c80bc14a0d8
CatOfJupit3r <catofjupit3r@gmail.com>
2024-06-02 19:15:18 +0300

    feat: add loading of custom lobby translation.

--- src/pages/GameRoomPage.tsx
+++ src/pages/GameRoomPage.tsx
@@ -4,17 +4,16 @@ import { useTranslation } from 'react-i18next'
 import GameLogicWrapper from '../components/GameLogicWrapper/GameLogicWrapper'
 import Overlay from '../components/Overlay/Overlay'
 import APIService from '../services/APIService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const GameRoomPage = () => {
+    const { lobbyId } = useParams()
     const [loadingTranslations, setLoadingTranslations] = useState(true)
     const { t, i18n } = useTranslation()
     const navigate = useNavigate()
 
     const loadTranslations = useCallback(async () => {
         const translations = await APIService.getTranslations([i18n.language, 'uk-UA'], ['builtins', 'nyrzamaer'])
-        if (!translations) throw new Error('Failed to load translations. Server response was empty.')
-        console.log("translations", translations)
         for (const language in translations) {
             if (translations[language] === null) continue
             for (const dlc in translations[language]) {
@@ -22,7 +21,12 @@ const GameRoomPage = () => {
                 i18n.addResourceBundle(language, dlc, translations[language][dlc], true, true)
             }
         }
-    }, [i18n])
+        if (lobbyId) {
+            const customTranslations = await APIService.getCustomLobbyTranslations(lobbyId)
+            i18n.addResourceBundle(i18n.language, "coordinator", customTranslations, true, true)
+        }
+
+    }, [i18n, lobbyId])
 
     useEffect(() => {
         try {
