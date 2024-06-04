60d1175bf4a9825aca97ac207828897fd6cf10e7
CatOfJupit3r <catofjupit3r@gmail.com>
2024-06-04 22:51:28 +0300

    feat: add option for <AttributeDisplay /> to include health, ap and defense attributes. add handling for cases when attributes are undefined or unknown, but accessed.

--- src/components/EntityDisplay/AttributeDisplay/AttributeDisplay.tsx
+++ src/components/EntityDisplay/AttributeDisplay/AttributeDisplay.tsx
@@ -1,57 +1,95 @@
-import { useMemo } from 'react'
-import { EntityAttributes } from '../../../models/Battlefield'
-import styles from './AttributeDisplay.module.css'
+import { useCallback, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
+import { EntityAttributes } from '../../../models/Battlefield'
 import capitalizeFirstLetter from '../../../utils/capitalizeFirstLetter'
+import styles from './AttributeDisplay.module.css'
 
-const AttributeDisplay = ({ attributes }: { attributes: EntityAttributes }) => {
+const AttributeDisplay = ({
+    attributes,
+    includeHealthAPDefense,
+}: {
+    attributes: EntityAttributes
+    includeHealthAPDefense?: boolean
+}) => {
     const { t } = useTranslation()
 
-    const ignoredAttributes = useMemo(
-        () => [
-            'builtins:current_health',
-            'builtins:max_health',
-            'builtins:current_action_points',
-            'builtins:max_action_points',
-            'builtins:current_armor',
-            'builtins:base_armor',
-        ],
-        []
+    const ignored = [
+        'builtins:current_health',
+        'builtins:max_health',
+        'builtins:current_action_points',
+        'builtins:max_action_points',
+        'builtins:current_armor',
+        'builtins:base_armor',
+    ] // we reset this on each render, thus no useMemo
+
+    const addIgnored = useCallback(
+        (attribute: string) => {
+            ignored.push(attribute)
+        },
+        [ignored]
     )
 
     const defense_attackAttributes = useMemo(() => {
         const found_attributes: { [key: string]: { attack: string; defense: string } } = {}
         for (const attribute in attributes) {
-            if (ignoredAttributes.includes(attribute)) continue
+            if (ignored.includes(attribute)) continue
             if (attribute.endsWith('_attack')) {
                 const attribute_name = attribute.slice(0, -7)
                 if (found_attributes[attribute_name]) {
                     found_attributes[attribute_name].attack = attributes[attribute]
                 } else {
-                    found_attributes[attribute_name] = { attack: attributes[attribute], defense: '' }
+                    found_attributes[attribute_name] = { attack: attributes[attribute], defense: '-' }
                 }
-                ignoredAttributes.push(attribute)
+                addIgnored(attribute)
             } else if (attribute.endsWith('_defense')) {
                 const attribute_name = attribute.slice(0, -8)
                 if (found_attributes[attribute_name]) {
                     found_attributes[attribute_name].defense = attributes[attribute]
                 } else {
-                    found_attributes[attribute_name] = { attack: '', defense: attributes[attribute] }
+                    found_attributes[attribute_name] = { attack: '-', defense: attributes[attribute] }
                 }
-                ignoredAttributes.push(attribute)
+                ignored.push(attribute)
             }
         }
         return found_attributes
     }, [attributes])
 
+    const healthAPDefenseValues = useMemo(() => {
+        return {
+            health: `${attributes['builtins:current_health'] || '-'}/${attributes['builtins:max_health'] || '-'}`,
+            actionPoints: `${attributes['builtins:current_action_points'] || '0'}/${attributes['builtins:max_action_points'] || '-'}`,
+            armor: `${attributes['builtins:current_armor'] || '-'}/${attributes['builtins:base_armor'] || '-'}`,
+        }
+    }, [attributes])
+
+    const HealthAPDefenseComponent = useCallback(() => {
+        return (
+            <>
+                <div className={styles.entityAttribute}>
+                    <p>{capitalizeFirstLetter(t('builtins:health'))}</p>
+                    <p>{healthAPDefenseValues.health}</p>
+                </div>
+                <div className={styles.entityAttribute}>
+                    <p>{capitalizeFirstLetter(t('builtins:action_points'))}</p>
+                    <p>{healthAPDefenseValues.actionPoints}</p>
+                </div>
+                <div className={styles.entityAttribute}>
+                    <p>{capitalizeFirstLetter(t('builtins:armor'))}</p>
+                    <p>{healthAPDefenseValues.armor}</p>
+                </div>
+            </>
+        )
+    }, [healthAPDefenseValues])
+
     return (
         <div>
+            {includeHealthAPDefense && <HealthAPDefenseComponent />}
             {Object.keys(attributes).map((attribute, index) => {
-                if (ignoredAttributes.includes(attribute)) return null
+                if (ignored.includes(attribute)) return null
                 return (
                     <div className={styles.entityAttribute} key={`common-attr-${index}`}>
                         <p key={`common-attr-name-${index}`}>{capitalizeFirstLetter(t(attribute))}</p>
-                        <p key={`common-attr-val-${index}`}>{attributes[attribute]}</p>
+                        <p key={`common-attr-val-${index}`}>{attributes[attribute] || '-'}</p>
                     </div>
                 )
             })}
@@ -60,7 +98,8 @@ const AttributeDisplay = ({ attributes }: { attributes: EntityAttributes }) => {
                     <div className={styles.entityAttribute} key={`special-attr-${index}`}>
                         <p key={`special-attr-name-${index}`}>{capitalizeFirstLetter(t(attribute))}</p>
                         <p key={`special-attr-val-${index}`}>
-                            {defense_attackAttributes[attribute].attack}/{defense_attackAttributes[attribute].defense}
+                            {defense_attackAttributes[attribute].attack || '-'}/
+                            {defense_attackAttributes[attribute].defense || '-'}
                         </p>
                     </div>
                 )
