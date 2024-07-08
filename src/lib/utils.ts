46bc4e0c6f9b68ea43b27ad3fc9624f8029436b9
catjofupit3r <catofjupit3r@gmail.com>
2024-07-09 01:39:13 +0300

    feat: add `applyPrefix` to easily add prefixes to TailwindCSS classes.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const applyPrefix = (prefix: string, className: ClassValue, includeTwoDots?: boolean): ClassValue => {
+    // adds a prefix to each TailwindCSS class in `className` params. Adds `:` in process.
+    if (!className) {
+        return className
+    }
+    let mapFunc: (value: string) => string
+    if (includeTwoDots) {
+        mapFunc = (value) => `${prefix}:${value}`
+    } else {
+        mapFunc = (value) => `${prefix}${value}`
+    }
+    return className
+        .toString()
+        .split(' ')
+        .map(mapFunc)
+        .reduce((acc, currentValue) => `${acc} ${currentValue}`, '')
+}
