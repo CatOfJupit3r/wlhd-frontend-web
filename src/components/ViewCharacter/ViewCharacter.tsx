d90f17f4d61d295cea2ee903ee9350c96ec77282
catjofupit3r <catofjupit3r@gmail.com>
2024-07-09 10:43:51 +0300

    chore: add `<ViewCharacter />` concept.

---
+++ src/components/ViewCharacter/ViewCharacter.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ViewCharacter = () => {
+    /*
+
+    We already have character info in our redux store! We just take it from there and render it as such.
+
+    Scrollable list with short info about characters.
+
+    {
+        AVATAR Name (@handle)
+        AVATAR Controlled by
+    }
+
+    The chosen character bg is darker than other and the component imitates the look of ingame display
+    (or I change ingame component and this to be the same question mark?)
+     */
+
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ViewCharacter;
