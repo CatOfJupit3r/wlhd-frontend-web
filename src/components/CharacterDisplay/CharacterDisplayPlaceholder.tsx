b9ba77d3d6471793ec5c329856db1b4fd4d7a825
CatOfJupit3r <catofjupit3r@gmail.com>
2024-08-02 20:52:05 +0300

    feat: implement `<CharacterDisplayPlaceholder />`.

--- src/components/CharacterDisplay/CharacterDisplayPlaceholder.tsx
+++ src/components/CharacterDisplay/CharacterDisplayPlaceholder.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { HTMLProps } from 'react'
+import { Skeleton } from '@components/ui/skeleton'
+import { Separator } from '@components/ui/separator'
 
-const CharacterDisplayPlaceholder = () => {
+const CharacterDisplayPlaceholder = (props: HTMLProps<HTMLDivElement>) => {
     return (
-        <div>
-
+        <div {...props}>
+            <div className="flex flex-row gap-4">
+                <Skeleton static={true} className={'size-20'} />
+                <div className={'flex flex-col gap-2'}>
+                    <Skeleton static={true} className={'h-6 w-32'} />
+                    <Skeleton static={true} className={'h-4 w-14'} />
+                    <Skeleton static={true} className={'h-5 w-48'} />
+                </div>
+            </div>
+            <Separator />
+            <div className={'flex flex-row justify-center gap-2'}>
+                <>
+                    {Array.from({ length: 6 }, (_, index) => (
+                        <Skeleton static={true} className={'h-8 w-9'} key={`skeleton-${index}`} />
+                    ))}
+                </>
+            </div>
+            <Separator />
+            <div className={'flex h-40 w-full flex-col gap-2'}>
+                {Array.from({ length: 5 }, (_, index) => (
+                    <Skeleton static={true} className={'h-6 w-full'} key={`skeleton-${index}`} />
+                ))}
+            </div>
         </div>
     )
 }
