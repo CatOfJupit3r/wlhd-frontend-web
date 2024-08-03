9ea242ba0f6157de04e267c8996ba203a241f1c1
CatOfJupit3r <catofjupit3r@gmail.com>
2024-08-03 21:28:00 +0300

    feat: add props for `<Progress />`: color of bars, size and direction of progress.

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,19 +2,41 @@ import * as ProgressPrimitive from '@radix-ui/react-progress'
 
 import { cn } from '@lib/utils'
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+import { ClassValue } from 'clsx'
+
+type ProgressProps = {
+    colored?: {
+        empty?: ClassValue
+        fill?: ClassValue
+    }
+    width?: ClassValue | 'w-full'
+    height?: ClassValue | 'h-4'
+    direction?: 'left-to-right' | 'up-to-down'
+}
 
 const Progress = forwardRef<
     ElementRef<typeof ProgressPrimitive.Root>,
-    ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+    ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & ProgressProps
+>(({ className, value, colored, width, height, direction, ...props }, ref) => (
     <ProgressPrimitive.Root
         ref={ref}
-        className={cn('relative h-4 w-full overflow-hidden rounded-full bg-secondary', className)}
+        className={cn(
+            'relative overflow-hidden rounded-full',
+            width || 'w-full',
+            height || 'h-4',
+            colored?.empty || 'bg-secondary',
+            className
+        )}
         {...props}
     >
         <ProgressPrimitive.Indicator
-            className="size-full flex-1 bg-primary transition-all"
-            style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+            className={cn('size-full flex-1 transition-all', colored?.fill || 'bg-primary')}
+            style={{
+                transform:
+                    direction === 'up-to-down'
+                        ? `translateY(${100 - (value || 0)}%)`
+                        : `translateX(-${100 - (value || 0)}%)`,
+            }}
         />
     </ProgressPrimitive.Root>
 ))
