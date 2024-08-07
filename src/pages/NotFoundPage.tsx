01127e2634f1c3ca9de2af77513535e1aa4dfbfc
CatOfJupit3r <catofjupit3r@gmail.com>
2024-08-07 18:51:28 +0300

    feat: add a sprinkle of styling to `<NotFoundPage />`.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom'
+import { GrDocumentMissing } from 'react-icons/gr'
+import { apprf, cn } from '@utils'
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <Link to={'/'}>Go to Home Page</Link>
+        <div className={'mt-[2vh] flex size-full flex-col items-center'}>
+            <div
+                className={cn(
+                    'flex w-full max-w-[50%] flex-col items-center gap-2',
+                    apprf('max-[712px]', 'max-w-full px-10')
+                )}
+            >
+                <h1>Page Not Found</h1>
+                <p className={'opacity-60 hover:opacity-100'}>
+                    The page you are looking for does not exist. <Link to={'/'}>Go to Home Page</Link>
+                </p>
+                <GrDocumentMissing className={'mt-5 text-t-bigger text-gray-400'} />
+            </div>
         </div>
     )
 }
 
-export { NotFoundPage as default }
+export default NotFoundPage
