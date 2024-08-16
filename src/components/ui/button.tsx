47632d003053ae5e36e9bfc4a3cc64175580657c
CatOfJupit3r <catofjupit3r@gmail.com>
2024-08-16 16:26:55 +0300

    feat: add new variant to button, `outlineToDefault`.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,6 +14,7 @@ const buttonVariants = cva(
                 default: 'bg-primary text-primary-foreground hover:bg-primary/90',
                 destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
                 outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+                outlineToDefault: 'border border-input bg-background hover:bg-primary/90 hover:text-primary-foreground',
                 secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 ghost: 'border border-transparent hover:bg-accent hover:text-accent-foreground',
                 destructiveGhost:
