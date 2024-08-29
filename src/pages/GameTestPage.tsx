62ba0f95a1171a97b37a9b764476471fc8bf3017
CatOfJupit3r <catofjupit3r@gmail.com>
2024-08-29 17:14:52 +0300

    refactor: update example_gamestate.json with new data.

--- src/pages/GameTestPage.tsx
+++ src/pages/GameTestPage.tsx
@@ -1,33 +1,42 @@
 import GameScreen from '@components/GameScreen/GameScreen'
 import { EntityInfoFull } from '@models/Battlefield'
+import { IndividualTurnOrder } from '@models/GameHandshake'
 import { setBattlefield } from '@redux/slices/battlefieldSlice'
 import { setControlledEntities, setEntityTooltips, setMessages, setTurnOrder } from '@redux/slices/infoSlice'
+import { selectLobbyId } from '@redux/slices/lobbySlice'
 import { resetTurnSlice, setEntityActions, setPlayersTurn } from '@redux/slices/turnSlice'
 import { AppDispatch } from '@redux/store'
 import APIService from '@services/APIService'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import example_gamestate from '../data/example_gamestate.json'
-import { IndividualTurnOrder } from '@models/GameHandshake'
 
 const GameTestPage = () => {
     const dispatch = useDispatch<AppDispatch>()
     const { i18n } = useTranslation()
     const [loadedGameState, setLoadedGameState] = useState(false)
+    const lobbyId = useSelector(selectLobbyId)
 
     useEffect(() => {
         dispatch(resetTurnSlice()) // to prevent any leftover state from previous games
     }, [])
 
     useEffect(() => {
-        // dispatch(setEntityTooltips(example_gamestate.tooltips) as any)
+        dispatch(setEntityTooltips(example_gamestate.tooltips) as any)
         dispatch(setMessages(example_gamestate.messages as any))
-        // dispatch(setControlledEntities(example_gamestate.controlledEntities as EntityInfoFull[]))
+        dispatch(setControlledEntities(example_gamestate.controlledEntities as EntityInfoFull[]))
         dispatch(setBattlefield(example_gamestate.battlefield as any))
-        // dispatch(setTurnOrder(example_gamestate.turnOrder as IndividualTurnOrder))
+        dispatch(setTurnOrder(example_gamestate.turnOrder as IndividualTurnOrder))
         dispatch(setEntityActions(example_gamestate.actions as any))
         dispatch(setPlayersTurn(true))
+        const loadCustom = async () => {
+            if (lobbyId) {
+                const customTranslations = await APIService.getCustomLobbyTranslations(lobbyId)
+                i18n.addResourceBundle(i18n.language, 'coordinator', customTranslations, true, true)
+            }
+        }
+        loadCustom().then()
         setTimeout(() => setLoadedGameState(true), 1000)
     }, [])
 
