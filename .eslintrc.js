ac909e728d87402fdc5c44f6d2f10681b6f1407d
CatOfJupit3r <catofjupit3r@gmail.com>
2024-09-11 20:28:00 +0300

    chore: add ignore patterns for `no-unused-vars` eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,14 @@ module.exports = {
     rules: {
         '@typescript-eslint/no-explicit-any': 'warn',
         'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+            },
+        ],
         'no-extra-semi': 'warn',
         '@typescript-eslint/no-extra-semi': 'warn',
     },
