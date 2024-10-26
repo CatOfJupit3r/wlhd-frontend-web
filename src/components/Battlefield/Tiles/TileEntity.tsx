a5d9638bcd362f4f03b0e1f785fa01023f3ce3a6
CatOfJupit3r <catofjupit3r@gmail.com>
2024-10-26 18:47:15 +0300

    feat(game): use shadcn.ui tooltip in TileEntity

--- src/components/Battlefield/Tiles/TileEntity.tsx
+++ src/components/Battlefield/Tiles/TileEntity.tsx
@@ -1,9 +1,10 @@
 import GameAsset from '@components/GameAsset'
-import { ImgHTMLAttributes, useMemo, useState } from 'react'
+import { ImgHTMLAttributes, useMemo } from 'react'
 import Decoration from './Decoration/Decoration'
 import EntityTooltip from './EntityTooltip/EntityTooltip'
 import styles from './Tiles.module.css'
 
+import { Tooltip, TooltipContent, TooltipTrigger } from '@components/ui/tooltip'
 import { useBattlefieldContext } from '@context/BattlefieldContext'
 import { cn, getCharacterSide } from '@utils'
 import { generateAssetPath } from '@utils/generateAssetPath'
@@ -23,49 +24,40 @@ const TileEntity = ({ square, className, ...props }: { square: string } & ImgHTM
 
     const squareInfo = useMemo(() => battlefield[square], [battlefield, square])
 
-    const [showTooltip, setShowTooltip] = useState(false)
-
-    const handleMouseEnter = () => {
-        setShowTooltip(true)
-    }
-
-    const handleMouseLeave = () => {
-        setShowTooltip(false)
-    }
-
     return (
-        <div
-            className={cn(
-                'relative',
-                squareInfo?.info !== null ? 'bg-cover' : '',
-                squareInfo?.flags.interactable ? 'cursor-pointer' : 'cursor-default',
-                'h-[10vh] w-[10vh]'
-            )}
-            onDoubleClick={() => {
-                if (squareInfo?.flags.interactable) {
-                    onClickTile(square)
-                }
-            }}
-            id={`square_${square}`}
-            style={{
-                backgroundImage: `url(/assets/local/default_battlefield/tile.png)`,
-            }}
-        >
-            <Decoration square={square} />
-            {squareInfo?.info.character?.decorations.sprite ? (
-                <GameAsset
-                    {...props}
-                    src={squareInfo?.info.character?.decorations.sprite}
-                    alt={t(squareInfo?.info.character?.decorations.name)}
-                    fallback={fallback}
-                    className={cn(styles.tile, className)}
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                />
-            ) : null}
-            {showTooltip &&
-                (squareInfo?.info.character ? <EntityTooltip character={squareInfo?.info.character} /> : null)}
-        </div>
+        <Tooltip>
+            <TooltipTrigger className={squareInfo?.flags.interactable ? 'cursor-pointer' : 'cursor-default'}>
+                <div
+                    className={cn('relative', squareInfo?.info !== null ? 'bg-cover' : '', 'h-[10vh] w-[10vh]')}
+                    onDoubleClick={() => {
+                        if (squareInfo?.flags.interactable) {
+                            onClickTile(square)
+                        }
+                    }}
+                    id={`square_${square}`}
+                    style={{
+                        backgroundImage: `url(/assets/local/default_battlefield/tile.png)`,
+                    }}
+                >
+                    <Decoration square={square} />
+                    {squareInfo?.info.character?.decorations.sprite ? (
+                        <GameAsset
+                            {...props}
+                            src={squareInfo?.info.character?.decorations.sprite}
+                            alt={t(squareInfo?.info.character?.decorations.name)}
+                            fallback={fallback}
+                            className={cn(styles.tile, className)}
+                        />
+                    ) : null}
+                </div>
+            </TooltipTrigger>
+            <TooltipContent
+                className={cn(`border-none bg-transparent p-0`, squareInfo?.info.character ? null : 'hidden')}
+                side={'right'}
+            >
+                {squareInfo?.info.character ? <EntityTooltip character={squareInfo?.info.character} /> : null}
+            </TooltipContent>
+        </Tooltip>
     )
 }
 
