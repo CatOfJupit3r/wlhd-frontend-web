2f0e5f4418447417228a9baa2f8e2f4f88e62730
CatOfJupit3r <catofjupit3r@gmail.com>
2024-10-26 23:18:36 +0300

    feat(ui): add better placeholder for lazy imports for headers and footer.

--- src/router/LayoutContextClient.tsx
+++ src/router/LayoutContextClient.tsx
@@ -1,16 +1,44 @@
+import { TooltipProvider } from '@components/ui/tooltip'
 import { CoordinatorEntitiesProvider } from '@context/CoordinatorEntitiesProvider'
 import { GameDataProvider } from '@context/GameDataProvider'
 import { useLayoutContext } from '@context/LayoutContext'
 import useIsLoggedIn from '@hooks/useIsLoggedIn'
 import paths from '@router/paths'
-import { refreshLobbyInfo, refreshUserInfo } from '@utils'
-import React, { lazy, ReactNode, startTransition, Suspense, useEffect } from 'react'
+import { apprf, cn, refreshLobbyInfo, refreshUserInfo } from '@utils'
+import { lazy, ReactNode, startTransition, Suspense, useEffect } from 'react'
 import { Navigate, Outlet, useParams } from 'react-router-dom'
-import { TooltipProvider } from '@components/ui/tooltip'
 
 const Header = lazy(() => import('@components/Header'))
 const Footer = lazy(() => import('@components/Footer'))
 const Notify = lazy(() => import('@components/Notify'))
+
+const HeaderPlaceholder = () => (
+    <div
+        className={cn(
+            'relative top-0 flex w-full justify-between bg-black p-4 text-t-normal text-white',
+            apprf('max-[512px]', 'flex-col justify-center gap-3 bg-black p-4 text-center align-middle')
+        )}
+    >
+        Loading header...
+    </div>
+)
+
+const FooterPlaceholder = () => (
+    <footer className={'relative bottom-0 box-border block min-h-fit w-full justify-center bg-[#252525FF] p-8'}>
+        <h1 className={'text-center text-t-normal font-bold text-[#d5d5d5]'}>
+            By player, for players... and developers too!
+        </h1>
+        <div
+            id={'footer-links'}
+            className={cn(
+                'mt-[1%] flex justify-center gap-[1%] text-t-small',
+                apprf('max-[512px]', 'flex flex-col items-center justify-center gap-2')
+            )}
+        >
+            <div>Loading footer...</div>
+        </div>
+    </footer>
+)
 
 const LayoutContextClient = () => {
     const { lobbyId } = useParams()
@@ -36,7 +64,7 @@ const LayoutContextClient = () => {
     return (
         <GlobalContext>
             {header && (
-                <Suspense fallback={<div>Loading header...</div>}>
+                <Suspense fallback={<HeaderPlaceholder />}>
                     <Header />
                 </Suspense>
             )}
@@ -46,7 +74,7 @@ const LayoutContextClient = () => {
                 </RouteProtection>
             </main>
             {footer && (
-                <Suspense fallback={<div>Loading footer...</div>}>
+                <Suspense fallback={<FooterPlaceholder />}>
                     <Footer />
                 </Suspense>
             )}
