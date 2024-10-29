4788481e341cd8c839b298615924d1ec68d56566
CatOfJupit3r <catofjupit3r@gmail.com>
2024-10-30 00:23:15 +0200

    feat(ui): add animations for turn queue

--- src/components/GameScreen/TurnOrderDisplay.tsx
+++ src/components/GameScreen/TurnOrderDisplay.tsx
@@ -3,7 +3,8 @@ import { CharacterGameAsset } from '@components/GameAsset'
 import { CharacterInTurnOrder } from '@models/GameModels'
 import { selectTurnOrder } from '@redux/slices/gameScreenSlice'
 import { cn, getCharacterSide } from '@utils'
-import React, { forwardRef, useState } from 'react'
+import { AnimatePresence, motion, MotionProps } from 'framer-motion'
+import { forwardRef, HTMLAttributes, Ref, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 
@@ -12,43 +13,40 @@ const CharacterCard = forwardRef(
         {
             character,
             isActive = false,
-            onMouseEnter,
-            onMouseLeave,
             className,
             ...props
         }: {
             character: CharacterInTurnOrder
             isActive?: boolean
-        } & React.HTMLAttributes<HTMLDivElement>,
-        ref: React.Ref<HTMLDivElement>
+        } & HTMLAttributes<HTMLDivElement> &
+            MotionProps,
+        ref: Ref<HTMLDivElement>
     ) => {
-        const [isHovered, setIsHovered] = useState<boolean>(false)
         const { t } = useTranslation()
-
         return (
-            <div
+            <motion.div
                 className={cn(
-                    'flex h-12 w-full flex-row gap-1 transition-all',
-                    isActive || isHovered ? 'opacity-100' : 'opacity-70 grayscale',
+                    'flex h-12 w-full flex-row gap-1 hover:opacity-100 hover:grayscale-0',
+                    isActive ? 'opacity-100' : 'opacity-70 grayscale',
                     className
                 )}
                 style={{
-                    transform: `translateX(-${isActive ? 0 : 10}%)`,
-                }}
-                onMouseEnter={(e) => {
-                    setIsHovered(true)
-                    if (onMouseEnter) {
-                        onMouseEnter(e)
-                    }
-                }}
-                onMouseLeave={(e) => {
-                    setIsHovered(false)
-                    if (onMouseLeave) {
-                        onMouseLeave(e)
-                    }
+                    transitionDuration: '150ms',
+                    transitionProperty: 'filter',
                 }}
                 title={t(character.decorations.name)}
                 ref={ref}
+                initial={{ x: -100, opacity: 0 }}
+                animate={{ x: isActive ? 0 : -10, opacity: 1 }} // active characters are a bit to the right
+                exit={{ x: 100, opacity: 0 }}
+                layout
+                transition={{
+                    layout: {
+                        type: 'tween',
+                        damping: 25,
+                        stiffness: 200,
+                    },
+                }}
                 {...props}
             >
                 <CharacterGameAsset
@@ -77,19 +75,72 @@ const CharacterCard = forwardRef(
                                 : 'bg-rose-400'
                     )}
                 ></div>
-            </div>
+            </motion.div>
         )
     }
 )
 
+type InnerInterOrderItem = {
+    character: CharacterInTurnOrder
+    isActive: boolean
+    key: string
+}
+
 const TurnOrderDisplay = () => {
     const turnOrder = useSelector(selectTurnOrder)
+    const [innerTurnOrder, setInnerTurnOrder] = useState<InnerInterOrderItem[]>([])
+
+    useEffect(() => {
+        // ask your god to explain this code
+        const endOfTurn = turnOrder.findIndex((character) => character === null)
+        const firstHalf = turnOrder
+            .slice(0, endOfTurn)
+            .filter((character) => character !== null)
+            .map((character, index) => ({
+                character,
+                isActive: index === 0,
+                key: character.descriptor,
+            }))
+        const secondHalf = turnOrder
+            .slice(endOfTurn + 1)
+            .filter((character) => character !== null)
+            .map((character) => ({
+                character,
+                isActive: false,
+            }))
+        const found = new Map<string, number>()
+
+        const newInnerTurnOrder = [...secondHalf, ...firstHalf].map(({ character, ...rest }) => {
+            const howManyFound = found.get(character.descriptor) ?? 0
+            found.set(character.descriptor, howManyFound + 1)
+            return {
+                ...rest,
+                character,
+                key: `${character.descriptor}-${howManyFound}`,
+            }
+        })
+        const howManyAfterEnd = secondHalf.length
+        let i = 0
+        const newInnerTurnOrderProcessed = [...newInnerTurnOrder]
+        while (i < howManyAfterEnd) {
+            const shifted = newInnerTurnOrderProcessed.shift()
+            if (shifted) {
+                newInnerTurnOrderProcessed.push(shifted)
+            }
+            i++
+        }
+        setInnerTurnOrder(newInnerTurnOrderProcessed as InnerInterOrderItem[])
+    }, [turnOrder])
 
     return (
         <BetterScrollableContainer>
-            {turnOrder.map((character, index) =>
-                character !== null ? <CharacterCard character={character} key={index} isActive={index === 0} /> : null
-            )}
+            <AnimatePresence>
+                {innerTurnOrder
+                    .filter((character) => character !== null)
+                    .map(({ key, character, isActive }) => (
+                        <CharacterCard key={key} character={character} isActive={isActive} />
+                    ))}
+            </AnimatePresence>
         </BetterScrollableContainer>
     )
 }
