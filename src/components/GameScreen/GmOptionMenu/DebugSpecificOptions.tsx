6e1df98373e9ac459e9ad958c4d4a7092acadff1
CatOfJupit3r <catofjupit3r@gmail.com>
2024-10-30 00:22:49 +0200

    feat(dx): add shiftturnorder button to test how animation work on queue changes

--- src/components/GameScreen/GmOptionMenu/DebugSpecificOptions.tsx
+++ src/components/GameScreen/GmOptionMenu/DebugSpecificOptions.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { CharacterInTurnOrder } from '@models/GameModels'
-import { AppDispatch } from '@redux/store'
-import { useDispatch } from 'react-redux'
-import { RandomUtils } from '@utils'
-import { setTurnOrder } from '@redux/slices/gameScreenSlice'
 import { Button } from '@components/ui/button'
+import { CharacterInTurnOrder } from '@models/GameModels'
+import { selectTurnOrder, setTurnOrder } from '@redux/slices/gameScreenSlice'
+import { AppDispatch } from '@redux/store'
+import { RandomUtils } from '@utils'
+import { useDispatch, useSelector } from 'react-redux'
 
 /*
 
@@ -12,97 +11,96 @@ This component is built for /game-test route and allows for better control over 
 
  */
 
-
 const MOCK_TURN_ORDER_CHARACTERS: Array<CharacterInTurnOrder> = [
     {
-        "descriptor" : "coordinator:hero",
-        "decorations" : {
-            "name" : "The Hero",
-            "description" : "An ordinary farmer who gained the strongest power capable of defeating The Demon King.",
-            "sprite" : "ally"
+        descriptor: 'coordinator:hero',
+        decorations: {
+            name: 'The Hero',
+            description: 'An ordinary farmer who gained the strongest power capable of defeating The Demon King.',
+            sprite: 'ally',
         },
-        "square" : {
-            "line" : 4,
-            "column" : 5
+        square: {
+            line: 4,
+            column: 5,
         },
-        "controlledByYou" : true,
-        "id_": '1'
+        controlledByYou: true,
+        id_: '1',
     },
     {
-        "descriptor" : "builtins:target_dummy",
-        "decorations" : {
-            "name" : "builtins:characters.target_dummy.name",
-            "description" : "builtins:characters.target_dummy.desc",
-            "sprite" : "builtins:fire"
+        descriptor: 'builtins:target_dummy',
+        decorations: {
+            name: 'builtins:characters.target_dummy.name',
+            description: 'builtins:characters.target_dummy.desc',
+            sprite: 'builtins:fire',
         },
-        "square" : {
-            "line" : 3,
-            "column" : 3
+        square: {
+            line: 3,
+            column: 3,
         },
-        "controlledByYou" : true,
-        "id_": '2'
+        controlledByYou: true,
+        id_: '2',
     },
     {
-        "descriptor" : "builtins:target_dummy",
-        "decorations" : {
-            "name" : "builtins:characters.target_dummy.name",
-            "description" : "builtins:characters.target_dummy.desc",
-            "sprite" : "builtins:unavailable"
+        descriptor: 'builtins:target_dummy',
+        decorations: {
+            name: 'builtins:characters.target_dummy.name',
+            description: 'builtins:characters.target_dummy.desc',
+            sprite: 'builtins:unavailable',
         },
-        "square" : {
-            "line" : 3,
-            "column" : 4
+        square: {
+            line: 3,
+            column: 4,
         },
-        "controlledByYou" : true,
-        "id_": '3'
+        controlledByYou: true,
+        id_: '3',
     },
     {
-        "descriptor" : "builtins:target_dummy",
-        "decorations" : {
-            "name" : "builtins:characters.target_dummy.name",
-            "description" : "builtins:characters.target_dummy.desc",
-            "sprite" : "builtins:wind"
+        descriptor: 'builtins:target_dummy',
+        decorations: {
+            name: 'builtins:characters.target_dummy.name',
+            description: 'builtins:characters.target_dummy.desc',
+            sprite: 'builtins:unavailable',
         },
-        "square" : {
-            "line" : 3,
-            "column" : 5
+        square: {
+            line: 3,
+            column: 4,
         },
-        "controlledByYou" : true,
-        "id_": '4'
+        controlledByYou: true,
+        id_: '3',
     },
     {
-        "descriptor" : "builtins:target_dummy_large",
-        "decorations" : {
-            "name" : "builtins:characters.target_dummy_large.name",
-            "description" : "builtins:characters.target_dummy_large.desc",
-            "sprite" : "builtins:target_dummy"
+        descriptor: 'builtins:target_dummy_large',
+        decorations: {
+            name: 'builtins:characters.target_dummy_large.name',
+            description: 'builtins:characters.target_dummy_large.desc',
+            sprite: 'builtins:target_dummy',
         },
-        "square" : {
-            "line" : 3,
-            "column" : 6
+        square: {
+            line: 3,
+            column: 6,
         },
-        "controlledByYou" : true,
-        "id_": '5'
+        controlledByYou: true,
+        id_: '5',
     },
     {
-        "controlledByYou": false,
-        "descriptor" : "builtins:target_dummy",
-        "decorations" : {
-            "name" : "builtins:characters.target_dummy.name",
-            "description" : "builtins:characters.target_dummy.desc",
-            "sprite" : "builtins:target_dummy"
+        controlledByYou: false,
+        descriptor: 'builtins:target_dummy',
+        decorations: {
+            name: 'builtins:characters.target_dummy.name',
+            description: 'builtins:characters.target_dummy.desc',
+            sprite: 'builtins:target_dummy',
         },
-        "square" : {
-            "line" : 3,
-            "column" : 2
+        square: {
+            line: 3,
+            column: 2,
         },
-        "id_": "6"
-    }
+        id_: '6',
+    },
 ]
 
 const RandomizeTurnOrder = () => {
     const dispatch = useDispatch<AppDispatch>()
-    
+
     const handleRandomizeButtonClick = () => {
         const newOrder = [...MOCK_TURN_ORDER_CHARACTERS]
         const shuffled = newOrder.sort(() => 0.5 - Math.random())
@@ -111,18 +109,38 @@ const RandomizeTurnOrder = () => {
             shuffled.pop()
         }
         const turnEndInx = RandomUtils.randNumber(0, shuffled.length - 1)
-        const turnOrder = shuffled.slice(turnEndInx).concat(shuffled.slice(0, turnEndInx))
-        dispatch(setTurnOrder(turnOrder))
+        // add null at turnEndInx
+        shuffled.splice(turnEndInx, 0, null as any)
+        // const turnOrder = shuffled.slice(turnEndInx).concat(shuffled.slice(0, turnEndInx))
+        dispatch(setTurnOrder(shuffled))
     }
 
     return <Button onClick={handleRandomizeButtonClick}>Randomize Turn Order</Button>
 }
 
+const ShiftTurnOrder = () => {
+    const dispatch = useDispatch<AppDispatch>()
+    const turnOrder = useSelector(selectTurnOrder)
+
+    const handleShiftButtonClick = () => {
+        const newOrder = [...turnOrder]
+        const first = newOrder.shift()
+        newOrder.push(first as any)
+        if (newOrder[0] === null) {
+            const second = newOrder.shift()
+            newOrder.push(second as any)
+        }
+        dispatch(setTurnOrder(newOrder))
+    }
+
+    return <Button onClick={handleShiftButtonClick}>Shift Turn Order</Button>
+}
 
 const DebugSpecificOptions = () => {
     return (
         <div>
             <RandomizeTurnOrder />
+            <ShiftTurnOrder />
         </div>
     )
 }
