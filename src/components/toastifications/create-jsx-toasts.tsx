031fe2c9941a108ccf7f0c11726b2c624e4f3437
CatOfJupit3r <catofjupit3r@gmail.com>
2025-02-04 23:39:59 +0200

    fix(ui): remove reference toasts syntaxis

--- src/components/toastifications/create-jsx-toasts.tsx
+++ src/components/toastifications/create-jsx-toasts.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import { toast } from 'react-toastify';
 
-import { GameMessageToast, GameMessageToastData } from './custom-jsx-toasts';
+import { ErrorToast, GameMessageToast, GameMessageToastData, InfoToast, InfoToastData } from './custom-jsx-toasts';
 import { CustomToastOptions } from './types';
 
 export const toastGameMessage = (message: ReactNode, options?: CustomToastOptions<GameMessageToastData>) => {
@@ -12,6 +12,29 @@ export const toastGameMessage = (message: ReactNode, options?: CustomToastOption
         position: 'bottom-left',
         closeOnClick: true,
         pauseOnHover: false,
+        ...options,
+    });
+};
+export const toastError = (title: string, message?: string, options?: CustomToastOptions<InfoToastData>) => {
+    toast(<ErrorToast />, {
+        data: {
+            title,
+            message,
+        },
+        position: 'top-center',
+        closeOnClick: true,
+        pauseOnHover: false,
+        ...options,
+    });
+};
+export const toastInfo = (title: string, message?: string, options?: CustomToastOptions<InfoToastData>) => {
+    toast(<InfoToast />, {
+        data: {
+            title,
+            message,
+        },
+        position: 'top-center',
+        closeOnClick: true,
         ...options,
     });
 };
--- src/components/toastifications/custom-jsx-toasts.tsx
+++ src/components/toastifications/custom-jsx-toasts.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { ToastBody, ToastDescription, ToastTitle } from './common-toast-parts';
-import { CustomToastFC } from './types';
+import { CustomReferenceToastFC, CustomToastFC } from './types';
 
 export type GameMessageToastData = {
     message: ReactNode;
@@ -23,4 +23,29 @@ const GameMessageToast: CustomToastFC<GameMessageToastData> = ({ data }) => {
     );
 };
 
-export { GameMessageToast };
+export type InfoToastData = {
+    title: string;
+    message?: string;
+};
+const InfoToast: CustomReferenceToastFC<InfoToastData> = ({ data }) => {
+    return (
+        <ToastBody className={'flex flex-col gap-2'}>
+            <ToastTitle className={'text-sm font-medium text-accent-foreground'}>{data?.title}</ToastTitle>
+            {data?.message ? <ToastDescription>{data?.message}</ToastDescription> : null}
+        </ToastBody>
+    );
+};
+export type ErrorToastData = {
+    title: string;
+    message?: string;
+};
+const ErrorToast: CustomReferenceToastFC<ErrorToastData> = ({ data }) => {
+    return (
+        <ToastBody className={'flex flex-col gap-2'}>
+            <ToastTitle className={'text-sm font-medium text-red-500'}>{data?.title}</ToastTitle>
+            {data?.message ? <ToastDescription>{data?.message}</ToastDescription> : null}
+        </ToastBody>
+    );
+};
+
+export { InfoToast, ErrorToast, GameMessageToast };
