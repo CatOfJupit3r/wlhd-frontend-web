8829b8fd329fd0ae1a637b3aa4cd1e3e55044d10
CatOfJupit3r <catofjupit3r@gmail.com>
2025-04-22 00:08:30 +0300

    feat(ui): add placeholders for achievements and detailed statistics profile tabs.

--- src/components/profile/profile-body/achievements/achievements.tsx
+++ src/components/profile/profile-body/achievements/achievements.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react';
+import { LuConstruction } from 'react-icons/lu';
 
 interface iAchievements {}
 
 const Achievements: FC<iAchievements> = () => {
-    return <div>Hello from Achievements!</div>;
+    return (
+        <div className="rounded-lg border bg-card p-6 text-card-foreground shadow">
+            <h3 className="mb-4 text-lg font-medium">Achievements</h3>
+            <div className="flex items-center justify-center gap-2">
+                <LuConstruction className="h-12 w-12" />
+                <p className="text-3xl font-medium">Under construction</p>
+            </div>
+        </div>
+    );
 };
 
 export default Achievements;
